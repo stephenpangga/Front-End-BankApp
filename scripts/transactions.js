c7f9e76fc5b89256282d956aee21e2d447bc1141
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2020-06-30 21:17:22 +0200

    fixing transaction order

--- scripts/transactions.js
+++ scripts/transactions.js
@@ -1,80 +1,84 @@
 const baseUrl = 'https://projectcodegenerationbankapp.herokuapp.com/api';
+//const baseUrl='http://localhost:8090/api';
 
 $(document).ready(() => {
+    let searchParams = new URLSearchParams(window.location.search);
+    if(!searchParams.has('iban'))
+    {
+        alert(`select a bank acount 1st`);
+        window.location.replace('transactionDashboard.html');
+    }
     // Fetch Transactions
     $.ajax({
-        url: baseUrl + '/transactions',
+        url: baseUrl + '/transactions' + '?iban=' + searchParams.get('iban'),
         type: 'GET',
         headers: { "Authorization": sessionStorage.getItem('token') },
         success: transactions => {
             $('#transactions').html('');
             transactions.forEach(transaction => {
-                $('#transactions').append(`Sender: ${transaction.sender.iban}: Current Balance: ${transaction.sender.balance}<br> Amount: €${transaction.amount} </br> 
+                $('#transactions').append(`Sender: ${transaction.sender.iban}: Current balance: ${transaction.sender.balance}<br> Amount: €${transaction.amount} </br> 
                 Recipient: ${transaction.recipient.iban} </br> </br>`);
+                //SHOW THE PASSED BANK ACCOUNT
+                //console.log(sessionStorage.getItem('token'));
             });
         },
         error: xhr => {
-            $('#transactions').html(`Something went wrong, please try again (HTTP Status Code ${xhr.status})`);
+            $('#transactions').html(`Something went wrong, please try again ${xhr.responseText})`);
         }
     });
+
 
     // Create transaction
     $('#transaction-form').submit(e => {
         e.preventDefault();
         $.ajax({
+            //to fetch the account of the sender
+            url: baseUrl + '/accounts/' + searchParams.get('iban'),
+            //url: baseUrl + '/accounts/NL01INHO0000000001',
             type: 'GET',
-            url: baseUrl + '/accounts/' + $('#transaction-recipient').val(),
-            headers: { "Authorization": sessionStorage.getItem('token') },
-            success: recipient => {
+            headers: {"Authorization": sessionStorage.getItem('token')},
+            success: sender => {
                 $.ajax({
-                    url: baseUrl + '/login/' + sessionStorage.getItem('token'),
                     type: 'GET',
+                    url: baseUrl + '/accounts/' + $('#transaction-recipient').val(),
                     headers: { "Authorization": sessionStorage.getItem('token') },
-                    success: user => {
+                    success: recipient => {
                         $.ajax({
-                            type: 'POST',
-                            url: baseUrl + '/transactions',
+                            url: baseUrl + '/login/' + sessionStorage.getItem('token'),
+                            type: 'GET',
                             headers: { "Authorization": sessionStorage.getItem('token') },
-                            contentType: 'application/json',
-                            data: JSON.stringify({
-                                sender: {
-                                    balance: 100.0,
-                                    currency: "Euro",
-                                    cumulativeTransaction: 5,
-                                    transactionAmoutLimit: 10000.0,
-                                    absoluteLimit: 10.0,
-                                    iban: "NL01INHO0000000001",
-                                    owner: {
-                                        id: 1,
-                                        email: "inholland@gmail.com",
-                                        password: "inhollandbank",
-                                        firstName: "Bank",
-                                        lastName: "bank",
-                                        accessLevel: "employee"
+                            success: user => {
+                                $.ajax({
+                                    type: 'POST',
+                                    url: baseUrl + '/transactions',
+                                    headers: { "Authorization": sessionStorage.getItem('token') },
+                                    contentType: 'application/json',
+                                    data: JSON.stringify({
+                                        sender: sender,
+                                        recipient: recipient,
+                                        amount: $('#transaction-amount').val(),
+                                        transactionType: 'transfer',
+                                        userPerforming: user
+                                    }),
+                                    success: () => {
+                                        $('#status').html(`€${$('#transaction-amount').val()} was successfully tranfered to ${$('#transaction-recipient').val()}`);
                                     },
-                                    accountType: "current"
-                                },
-                                recipient: recipient,
-                                amount: $('#transaction-amount').val(),
-                                transactionType: 'transfer',
-                                userPerforming: user
-                            }),
-                            success: () => {
-                                $('#status').html(`€${$('#transaction-amount').val()} was successfully tranfered to ${$('#transaction-recipient').val()}`);
+                                    error: xhr => {
+                                        $('#status').html(`Something went wrong, please try again - ${xhr.responseText})`);
+                                    }
+                                });
                             },
                             error: xhr => {
-                                $('#status').html(`Something went wrong, please try again (HTTP Status Code ${xhr.status})`);
+                                $('#status').html(`Something went wrong, please try again - ${xhr.responseText})`);
                             }
                         });
                     },
                     error: xhr => {
-                        $('#status').html(`Something went wrong, please try again (HTTP Status Code ${xhr.status})`);
+                        $('#status').html(`Something went wrong, please try again - ${xhr.responseText})`);
                     }
                 });
-            },
-            error: xhr => {
-                $('#status').html(`Something went wrong, please try again (HTTP Status Code ${xhr.status})`);
             }
         });
+        
     });
 });
